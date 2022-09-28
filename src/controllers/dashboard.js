00eb0a4693f3b7e194c910d42d94330938b35d10
kongcheexiong <xiongkongchee@gmail.com>
2022-09-28 21:11:43 +0700

    sdf

--- src/controllers/dashboard.js
+++ src/controllers/dashboard.js
@@ -1,45 +1,43 @@
 const News = require("../models/news.model");
 const NewsCate = require("../models/news_cate.model");
+const { find } = require("../models/user.model");
 const User = require("../models/user.model");
-const userType = require("../models/userType.model");
+const UserType = require("../models/userType.model");
 
 module.exports = {
   fetchData: async (req, res) => {
     const news = await News.find();
     const newsCate = await NewsCate.find();
     const user = await User.find();
-    const userType = await User.find();
-
-  
+    const userType = await UserType.find();
 
     const countUserGroupByDate = await User.aggregate([
       {
         $group: {
-            "_id":{
-                    "$dateToString":{"format":"%Y-%m-%d","date":"$createAt"}
-                  },
-                //   "first":{
-                //     "$first":"$$ROOT"
-                //   },
+          _id: {
+            $dateToString: { format: "%Y-%m-%d", date: "$createAt" },
+          },
           distinctCount: { $sum: 1 },
-          
+            // adsfds: { $count: {}},
         },
       },
-      {$sort: { createAt : -1}}
+      { $sort: { _id: 1 } },
     ]);
-    console.log("countUserGroupByDate==>",countUserGroupByDate)
+    console.log("countUserGroupByDate==>", countUserGroupByDate);
     const countNewsGroupByDate = await News.aggregate([
-        {
-          $group: {
-              "_id":{
-                      "$dateToString":{"format":"%Y-%m-%d","date":"$createAt"}
-                    },
-            distinctCount: { $sum: 1 },
+      {
+        $group: {
+          _id: {
+            $dateToString: { format: "%Y-%m-%d", date: "$createAt" },
           },
+
+          distinctCount: { $sum: 1 },
         },
-      ]);
-   
-    // console.log(countUserGroupByDate);
+      },
+      { $sort: { _id: 1 } },
+    ]);
+
+    console.log("countNewsGroupByDate==>", countNewsGroupByDate);
 
     return res.status(200).json({
       news,
@@ -47,7 +45,7 @@ module.exports = {
       user,
       userType,
       countUserGroupByDate,
-      countNewsGroupByDate
+      countNewsGroupByDate,
     });
   },
 };
